95e680c3b683db06a7f3ded4ca0b220d21568def
Daisy Koech <koechdaisy86@gmail.com>
2026-04-14 11:08:09 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Write your code here!
+const url = "https://jsonplaceholder.typicode.com/posts"
+
+fetch(url)
+.then(response => response.json())
+.then(posts => {
+    console.log(posts);
+})
+.catch(error => {
+    console.error("Error fetching data:", error)
+})
+
+function displayPosts(posts) {
+    const postList = document.getElementById("post-list")
+
+    posts.forEach((post) => {
+        const listItem = document.createElement("li");
+
+        const heading = document.createElement("h1");
+        heading.textContent = `${post.title}`
+        
+        const body =document.createElement("p");
+        body.textContent = `${post.body}`
+
+        listItem.appendChild(heading);
+        listItem.appendChild(body);
+
+        postList.appendChild(listItem);
+    })
+}
+
+fetch(url)
+.then(response => response.json())
+.then(posts => {
+    displayPosts(posts);
+})
+
+
+//using async/await
+async function fetchPosts() {
+    try {
+        const response = await fetch(url)
+        const posts = await response.json()
+        console.log(posts)
+    }
+    catch (error){
+        console.error("Error fetching data:", error);
+    }
+}
+fetchPosts();
+ 
